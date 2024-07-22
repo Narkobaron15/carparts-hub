9370e34f1c04f27fd83a7b79f208de8751dadbe9
Alex Ivanov <oleksii120@gmail.com>
2024-07-22 18:00:21 +0300

    Added layout with redux

---
+++ frontend/lib/store.ts
@@ -0,0 +1,15 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+
+export const makeStore = () => {
+    return configureStore({
+        reducer: combineReducers({
+            // Add reducers here
+        }),
+    })
+}
+
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
